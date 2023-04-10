347c3bacb5f4ecf926ad246b2553b59f029bb1e5
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2023-04-10 15:08:49 -0400

    Working on Home page, greeting component finished, working on hero component

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Greeting from '../components/Greeting';
+import Hero from '../components/Hero';
+
+function Home() {
+  return (
+    <div>
+      <Greeting />
+      <Hero />
+    </div>
+  );
+}
+
+export default Home;
