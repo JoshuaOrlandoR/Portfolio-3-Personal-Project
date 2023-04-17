af694399b46001b89ce5bb263627436fe34655b7
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2023-04-17 16:58:25 -0400

    Fixed nav styling, small changes to social media component

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -10,11 +10,11 @@ const SocialMedia = () => {
     return (
         <div className="social-media-component">
             <div className="title-container">
-                <h3 className="title-monospace">You Can Reach Me at the Following</h3>
+                <h3 className="title-monospace">Reach out and stay connected with me @</h3>
             </div>
             <div className="icons-container">
-                <i className="devicon-github-original social-media-icon" onClick={() => handleClick('https://github.com/yourusername')}></i>
-                <i className="devicon-linkedin-plain social-media-icon" onClick={() => handleClick('https://www.linkedin.com/in/yourusername/')}></i>
+                <i className="devicon-github-original social-media-icon" onClick={() => handleClick('https://github.com/JoshuaOrlandoR')}></i>
+                <i className="devicon-linkedin-plain social-media-icon" onClick={() => handleClick('https://www.linkedin.com/in/joshua-ramkissoon-140650265/')}></i>
                 <div className="resume-container" onClick={() => handleClick('https://example.com/your_resume.pdf')}>
                     <AiFillFileAdd className="resume-icon social-media-icon" />
                     <span className="resume-text">CV yet to add</span>
